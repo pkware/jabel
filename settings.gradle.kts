79976402e984bdf81527148d65f4d9b4a3e5040c
Marius Volkhart <marius.volkhart@pkware.com>
2026-06-22 10:30:07 -0400

    Upgrade to Develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,15 +19,16 @@ pluginManagement {
 }
 
 plugins {
-    `gradle-enterprise`
+    id("com.gradle.develocity") version "4.4.3"
 }
 
 val isCiServer = System.getenv().containsKey("CI")
 
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree.set("yes")
+        publishing.onlyIf { _ -> false }
         if (isCiServer) {
             tag("CI")
         }
